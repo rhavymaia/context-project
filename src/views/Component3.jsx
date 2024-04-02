5ce0ae00369fb194f60f4ec33d9c7a906d624216
Rhavy Maia Guedes <rmg3@cin.ufpe.br>
2024-04-02 15:01:20 -0300

    Remoção de components não utilizados.

--- src/views/Component3.jsx
+++
@@ -1,8 +0,0 @@
-import { useUser } from './UserContext';
-
-const Component3 = () => {
-  let { show } = useUser();
-  return <div>O valor da variável show é {`${show}`}</div>;
-};
-
-export default Component3;
